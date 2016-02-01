24b119a175d65be7ecc0cf32dd9ed3e359cd6bc4
Vasilis Papavasileiou <git@vasilis.airpost.net>
2016-02-01 12:37:45 +0100

    Enable all ocsigen plugins

--- bin/main.ml
+++ bin/main.ml
@@ -1,7 +1,11 @@
 let () = Pa_macro.linkme
 let () = Pa_ounit.linkme
 let () = Pa_bench.linkme
-let () = Pa_type_conv.linkme
+(* Pa_type_conv conflicts with Pa_deriving *)
+(* let () = Pa_type_conv.linkme *)
 let () = Pa_js.linkme
+let () = Pa_eliom.linkme
+let () = Pa_lwt.linkme
+let () = Pa_deriving.linkme
 
 let () = Camlp4_to_ppx.main ()
